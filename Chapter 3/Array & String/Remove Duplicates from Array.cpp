b7d8ba8855609ca292aa01e9e13d30a1642b8aae
ranamahmud <ranamd0@gmail.com>
2022-02-06 06:47:06 +0600

    Remove Duplicates from Array with extra space done.

---
+++ Chapter 3/Array & String/Remove Duplicates from Array.cpp
@@ -0,0 +1,30 @@
+// source: leetcode.com https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // using extra space , space O(n)
+    int removeDuplicatesExtraSpace(vector<int>& nums) {
+      vector<int> unique_nums;
+      unique_nums.push_back(nums[0]);
+      for(int i = 1; i< nums.size(); i++)
+        if(nums[i]!=nums[i-1])
+            unique_nums.push_back(nums[i]);
+    for(int i = 0; i< unique_nums.size(); i++)
+        cout<<unique_nums[i]<<" ";
+    cout<<endl;
+      return unique_nums.size();
+    }
+};
+int main(){
+    Solution solution;
+    vector<int> ar({1,1,2,3,3});
+    int n = solution.removeDuplicatesExtraSpace(ar);
+    cout<<n<<endl;
+    for(int i = 0; i< n; i++)
+        cout<<ar[i]<<" ";
+    cout<<endl;
+
+    return 0;
+}
